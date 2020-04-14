2e254960444ecbe4a40f752780c23156c733032b
Lee-Roy King <leeroywking@gmail.com>
2020-04-14 11:36:57 -0700

    iterative solution

--- json_diff.py
+++ json_diff.py
@@ -1,9 +1,6 @@
 import json
 import sys
 
-with open("./simple.json") as file1:
-    json_simple = json.load(file1)
-
 with open(sys.argv[1]) as file1:
     json1 = json.load(file1)
 
@@ -23,46 +20,58 @@ def get_by_path(dict_to_check, list_of_keys):
         return "ERROR: Value is undefined"
 
 
-def depth_first_search(current, compare, pathlist=[]):
+def print_message(current, compare, current_path_list):
+    delimiter = "]["
+    path_str = delimiter.join(str(x) for x in current_path_list)
+    print(f"json[{path_str}]")
+    print("\033[36m" + f"< {current}", "\033[0m")
+    print("\033[95m" + f"> {get_by_path(compare, current_path_list)}" + "\033[0m")
+    print("---")
+    print("")
+
+
+def depth_first_search(current, compare, pathlist):
     global string_and_int_count
     global error_count
-    current_path_list = pathlist.copy()
+    global stack
+
     if isinstance(current, str) or isinstance(current, int):
         try:
             string_and_int_count += 1
             if current == get_by_path(compare, pathlist):
-                pathlist.pop()
+                pass
             else:
-                delimiter = "]["
-                fancy_str = delimiter.join(str(x) for x in pathlist)
-                print(f"json[{fancy_str}]")
-                print("\033[36m" + f"< {current}", "\033[0m")
-                print("\033[95m" + f"> {get_by_path(compare, pathlist)}" + "\033[0m")
-                print("---")
-                print("")
-                pathlist.pop()
+                print_message(current, compare, pathlist)
+
         except:
-            print(pathlist)
-            pathlist.pop()
+            print(current_path_list)
             error_count += 1
             if error_count > 5:
                 exit()
 
     elif isinstance(current, list):
+        current.reverse()  # so items stack in correct order
         for index, item in enumerate(current):
-            current_path_list.append(index)
-            depth_first_search(item, compare, current_path_list)
-        if len(pathlist) > 0:
-            pathlist.pop()
+            current_path_list = pathlist.copy()
+            current_path_list.append(len(current) - index -1) # reverse index
+            stack.append((item, compare, current_path_list))
 
     elif isinstance(current, dict):
-        for key in current:
+        for key in reversed(list(current.keys())):  # so they stack properly
+            current_path_list = pathlist.copy()
             current_path_list.append(key)
-            depth_first_search(current[key], compare, current_path_list)
-        if len(pathlist) > 0:
-            pathlist.pop()
+            stack.append((current[key], compare, current_path_list))
 
 
-depth_first_search(json1, json2)
+stack = []
+stack.append((json1, json2, []))
+
+print("")
+while len(stack) > 0:
+    (item, compare, path_list) = stack.pop()
+    current_path_list = path_list.copy()
+    depth_first_search(item, compare, current_path_list)
+
+
 print(f"{error_count} Errors")
 print(f"{string_and_int_count} Values checked")
